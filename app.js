89db6716dc3a591e4047e47212cc4dd46060f2b3
Mirisaurio <Mirisaurio>
2024-01-25 15:35:13 -0600

    genera número aleatorio con Math.random

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-    let numeroSecreto = 6;
+    let numeroSecreto = Math.floor(Math.random()*10)+1
     let numberUser = 0;
     let intentos = 1;
     //let palabraIntento = "intento";
     let maximosIntentos = 3;
+    console.log(numeroSecreto);
     //se añade loop/bucle para decir que mientras la condición de numero usuario es distinta a numero secreto se va a repetir el juego
     while (numberUser != numeroSecreto) { 
     //método para mostrar un cuadro de diálogo que le pide al usuario que ingrese información.
